c169f06de04ab0c2029948b4b52a1dad0119e316
Noel <noeltrejom16@gmail.com>
2022-11-28 08:01:47 -0700

    desde casa

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const {getItems,getItem,createItem} = require("../controllers/tracks")
 
 
-
-router.get('/',(req,res)=>{
-    res.json("Bienvenido a Tracks");
-});
+router.get('/',getItems);
+router.get('/:id',getItem);
+router.post('/',createItem);
 
 
 module.exports = router
